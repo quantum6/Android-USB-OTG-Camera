d2ed39e4180de46a3deeb610348da51bce67e578
quantum6 <quantum6@yeah.net>
2023-07-08 09:36:59 +0800

    Android 12 and above

--- libusbcamera/src/main/java/com/serenegiant/usb/USBMonitor.java
+++ libusbcamera/src/main/java/com/serenegiant/usb/USBMonitor.java
@@ -178,7 +178,8 @@ public final class USBMonitor {
 			if (DEBUG) Log.i(TAG, "register:");
 			final Context context = mWeakContext.get();
 			if (context != null) {
-				mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+				mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION),
+						PendingIntent.FLAG_IMMUTABLE);
 				final IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 				// ACTION_USB_DEVICE_ATTACHED never comes on some devices so it should not be added here
 				filter.addAction(ACTION_USB_DEVICE_ATTACHED);
